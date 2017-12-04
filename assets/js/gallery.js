a0a8cedf91b13e3e7d6977c2c1cc716fe9dc9af7
Izæt <izet0004@elev.techcollege.dk>
2017-12-04 21:08:24 +0100

    Galleri visning V0.01 :100:

---
+++ assets/js/gallery.js
@@ -0,0 +1,23 @@
+function gallerySwitch(elementName) { // Simple as fuck galleri
+    switch (elementName) {
+        case "Tomrer":
+            hideShow(elementName);
+            break;
+        case "Murer":
+            hideShow(elementName);
+            break;
+        default:
+            break;
+    }
+}
+
+function hideShow(name) {
+    $(".gallery picture").each(function () {
+        if ($(this).hasClass(name)) {
+            $(this).fadeIn()
+        } else {
+            $(this).fadeOut();
+        }
+    });
+}
+// For hver ITEM der ikke har class TØMRER i GALLERY append class HIDE
