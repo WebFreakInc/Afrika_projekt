1e1eaf84eb3c41ca334e74e93f93ed89ee81ce65
Izæt <izet0004@elev.techcollege.dk>
2017-12-05 13:03:09 +0100

    Test

--- gallery.php
+++ gallery.php
@@ -1,21 +1,28 @@
 <!-- Header  -->
 <?php require ("assets/incl/head.php") ?>
 
-<!-- Billeder med class fx tømrer, sorter efter class,  -->
+Case, på START hvor DIVERSE bliver vist
+
 
 <section class="container">
-    <a name="Tomrer" id="" class="btn btn-primary" href="#" onclick="gallerySwitch(this.name)" role="button">Tømrer</a>
-    <a name="Murer" id="" class="btn btn-primary" href="#" onclick="gallerySwitch(this.name)" role="button">Murer</a>
+    <!-- <a name="Tomrer" id="" class="btn btn-primary" href="#" onclick="gallerySwitch(this.name)" role="button">Tømrer</a>
+    <a name="Murer" id="" class="btn btn-primary" href="#" onclick="gallerySwitch(this.name)" role="button">Murer</a> -->
+    <div class="form-group">
+      <select class="form-control" name="category" oninput="gallerySwitch(this.value)" id="">
+        <option value="Tomrer">Tomrer</option>
+        <option value="Murer">Murer</option>
+      </select>
+    </div>
     <div class="row gallery">
         <picture class="col-lg-3 Murer" style="">
             <img class="img-fluid" width="" src="assets/img/productImages/1.jpg" alt="" srcset="">
             <h4>Brød</h4>
         </picture>
-        <picture class="col-lg-3 Tomrer" style="">
+        <picture class="col-lg-3 Tømrer" style="">
             <img class="img-fluid" width="" src="assets/img/productImages/2.jpg" alt="" srcset="">
             <h4>Brød</h4>
         </picture>
-        <picture class="col-lg-3 Tomrer" style="">
+        <picture class="col-lg-3 Tømrer" style="">
             <img class="img-fluid" width="" src="assets/img/productImages/3.jpg" alt="" srcset="">
             <h4>Brød</h4>
         </picture>
